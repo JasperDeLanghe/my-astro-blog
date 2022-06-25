0007279b62fa325cc2ff5d78bf35377e1a8f815b
Jasper <jasper.delanghe@gmail.com>
2022-06-25 13:40:35 +0200

    setup netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import netflify from "@astrojs/netflify";
 
+import preact from '@astrojs/preact';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netflify(),
   integrations: [preact(), react(), tailwind()]
 });
