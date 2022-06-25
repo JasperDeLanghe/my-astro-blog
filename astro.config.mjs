8635bd1cf2a563f83ddfc6315500aaaf264ca006
Jasper <jasper.delanghe@gmail.com>
2022-06-25 13:27:24 +0200

    Added react & tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+  integrations: [preact(), react(), tailwind()]
 });
